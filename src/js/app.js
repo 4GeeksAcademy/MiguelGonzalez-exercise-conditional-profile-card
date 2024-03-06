3a57c617406c883a3ecd6f3681e4b0980fefcf83
Miguel González <152456005+Miguel-Gonzalez01@users.noreply.github.com>
2024-03-06 20:03:33 +0000

    Final project

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,10 @@
+import { name } from "file-loader";
 import "../style/index.css";
 
 /**
  *  EDIT ONLY INSIDE THIS RENDER FUNCTION
  *  This function is called every time the user changes types or changes any input
- * 
+ *
     {
         includeCover: true, // if includeCover is true the algorithm should show the cover image
         background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the image's url that will be used as a background for the profile cover
@@ -15,7 +16,7 @@ import "../style/index.css";
         linkedin: null,
         instagram: null,
 
-        name: null,
+        name: null ,
         lastName: null,
         role: null,
         country: null,
@@ -33,14 +34,14 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1 placeholder="">${variables.name} ${variables.lastName}</h1>
+          <h2>${variables.role}</h2>
+          <h3>${variables.city}, ${variables.country}</h3>
+          <ul class=${variables.socialMediaPosition}>
+            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a>${variables.twitter}</li>
+            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a>${variables.github}</li>
+            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a>${variables.linkedin}</li>
+            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a>${variables.instagram}</li>
           </ul>
         </div>
     `;
@@ -56,19 +57,19 @@ window.onload = function() {
     // this is the image's url that will be used as a background for the profile cover
     background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL: "pfp.jpg",
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
-    twitter: null,
-    github: null,
-    linkedin: null,
-    instagram: null,
-    name: null,
-    lastName: null,
-    role: null,
-    country: null,
-    city: null
+    twitter: "Your @",
+    github: "Your User",
+    linkedin: "Your User",
+    instagram: "Your User",
+    name: "Name",
+    lastName: "Last Name",
+    role: "Your Role",
+    country: "Country",
+    city: "City"
   };
   render(window.variables); // render the card for the first time
 
